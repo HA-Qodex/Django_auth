0d9e9ee04873059b1ef91b100435227adafd3dfb
Huzaifa Ahmed <huzaifa.ahmed33@gmail.com>
2022-02-27 17:35:42 +0600

    initial commit

---
+++ django_blog/blog_app/models.py
@@ -0,0 +1,51 @@
+import password as password
+import phone as phone
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.utils.translation import gettext_lazy as _
+from django.db import models
+from django.utils import timezone
+
+
+class CustomAccountManager(BaseUserManager):
+    def create_superuser(self, email, username, phone, password, **other_fields):
+        other_fields.setdefault('is_admin', True)
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must be assigned to is_staff = True.')
+        if other_fields.get('is_admin') is not True:
+            raise ValueError('Superuser must be assigned to is_admin = True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must be assigned to is_superuser = True.')
+
+        return self.create_user(email, username, phone, password, **other_fields)
+
+    def create_user(self, email, username, phone, password, **other_fields):
+
+        if not email:
+            raise ValueError(_('You must provide an email address'))
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, phone=phone, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class NewUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150)
+    phone = models.CharField(max_length=11, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    created_on = models.DateTimeField(default=timezone.now)
+
+    objects = CustomAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'phone']
+
+    def __str__(self):
+        return self.email
---
+++ django_blog/blog_app/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import *
+
+
+class Registration(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        try:
+            if serializer.is_valid():
+                serializer.save()
+                response_message = {
+                    "success": True,
+                    "message": "User has been registered",
+                    "data": "",
+                    "error": "",
+                    "error_code": 200
+                }
+                return Response(response_message)
+            else:
+                response_message = {
+                    "success": False,
+                    "message": "User registration failed",
+                    "data": "",
+                    "error": serializer.errors[next(iter(serializer.errors))][0],
+                    "error_code": 401
+                }
+                return Response(response_message)
+        except Exception as e:
+            response_message = {
+                "success": False,
+                "message": str(e),
+                "data": "",
+                "error": "",
+                "error_code": 400
+            }
+        return Response(response_message)
